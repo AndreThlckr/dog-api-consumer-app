21f8ed80fdc90e509f3b6992e5c312da4db4bdb0
AndreThlckr <andretheilacker@gmail.com>
2020-01-21 09:13:57 -0300

    WIP: adicionar funcionalidade de adicionar e listar favoritos.

--- app/src/main/java/com/example/dogeapp/BreedAdapter.java
+++ app/src/main/java/com/example/dogeapp/BreedAdapter.java
@@ -9,8 +9,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.squareup.picasso.Picasso;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -46,12 +44,13 @@ public class BreedAdapter extends RecyclerView.Adapter<BreedAdapter.BreedsViewHo
 
     @Override
     public void onBindViewHolder(@NonNull BreedsViewHolder holder, int position) {
-        String breedName = breeds.get(position).getBreedName();
+        Breed breed = breeds.get(position);
+        String breedName = breed.getBreedName();
 
         breedName = breedName.substring(0, 1).toUpperCase() + breedName.substring(1);
 
         holder.textBreedName.setText(breedName);
-
+        holder.setAsFavorite(breed.isFavorite());
     }
 
     public class BreedsViewHolder extends RecyclerView.ViewHolder {
@@ -62,14 +61,14 @@ public class BreedAdapter extends RecyclerView.Adapter<BreedAdapter.BreedsViewHo
         public BreedsViewHolder(@NonNull View itemView) {
             super(itemView);
 
+            Breed breed = breeds.get(getAdapterPosition());
+
             textBreedName = itemView.findViewById(R.id.txt_name);
             favoriteButton = itemView.findViewById(R.id.ItemListStar);
 
             textBreedName.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    breeds.get(getAdapterPosition());
-
                     if(itemClickListener != null) {
                         itemClickListener.onItemClick(getAdapterPosition());
                     }
@@ -79,15 +78,19 @@ public class BreedAdapter extends RecyclerView.Adapter<BreedAdapter.BreedsViewHo
             favoriteButton.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    breeds.get(getAdapterPosition());
-
-                    favoriteButton.setImageResource(R.drawable.ic_star_checked_24dp);
-
                     if(itemClickListener != null) {
                         itemClickListener.onFavoriteClick(getAdapterPosition());
                     }
                 }
             });
+        }
+
+        protected void setAsFavorite(boolean isFavorite){
+            if(isFavorite){
+                favoriteButton.setImageResource(R.drawable.ic_star_checked_24dp);
+            } else {
+                favoriteButton.setImageResource(R.drawable.ic_star_unchecked_24dp);
+            }
         }
     }
 }
